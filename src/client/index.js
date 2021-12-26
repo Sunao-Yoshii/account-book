4dd09aa45e2b2ac640a44e780200675f2ba74665
Sunao-Yoshii <2810066+Sunao-Yoshii@users.noreply.github.com>
2021-12-26 12:30:55 +0900

    いったん削除

--- src/client/index.js
+++
@@ -1,7 +0,0 @@
-
-import { createElement } from 'lwc';
-import EntryApp from 'book/app';
-
-const app = createElement('account-book', { is: EntryApp });
-// eslint-disable-next-line @lwc/lwc/no-document-query
-document.querySelector('#main').appendChild(app);
